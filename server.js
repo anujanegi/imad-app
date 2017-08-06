c3665aecb44a315726f1207fd8f9d77eb3cdac12
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-06 12:33:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
   
-  return htmlTemplate; 
 });
 
 
