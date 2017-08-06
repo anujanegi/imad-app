a0b1b1d31d9685ce2c33610901afd5aee30c8f87
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-07 00:24:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var ele = document.getElementById('main-hi');
 ele.innerHTML='hi!';
+var img=documnet.getElementById('o');
+img.onclick=function(){
+    img.style.marginLeft="100px";
+}
