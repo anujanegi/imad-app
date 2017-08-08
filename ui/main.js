8f2e74ec447d5054af32981bb025339b234c388d
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-09 00:22:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,3 +32,18 @@ function moveRight(){
 img.onclick=function(){
     var m=setInterval(moveRight,50);
 }*/
+
+
+//submit name
+var n=documnet.getElementById('name');
+var name=n.value;
+var submit=document.getElementIdBy('submit_btn');
+submit_btn.onclick=funtion(){
+  var names=['name1','name2','name3'];
+  var list='';
+  for(var i=0;i<names.length;i++){
+      list+='<li>+names[i]+</li>';
+  }
+  var ul=document.getElementById('list');
+  ul.innerHTML=list;
+};
