cbf5fc1c7a2004ebb222b10e724c2d006986fe4b
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-09 12:17:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,12 +38,25 @@ img.onclick=function(){
 var n=documnet.getElementById('name');
 var nam=n.value;
 var submit=document.getElementIdBy('submit_btn');
-submit.onclick=funtion(){
-  var names=['name1','name2','name3','name4'];
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list+='<li>+names[i]+</li>';
-  }
-  var ul=document.getElementById('list');
-  ul.innerHTML=list;
+
+submit.onclick=function(){
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
+            {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>+names[i]+</li>';
+                }
+                var ul=document.getElementById('list');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET','http://anujanegi1.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
