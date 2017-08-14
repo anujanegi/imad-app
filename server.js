ad82443d768c714ff51468d20f9dbbfcf70d7c3d
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-15 02:01:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+    user:'anujanegi1',
+    database:'anujanegi1',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -88,6 +97,17 @@ function createTemplate(data){
   
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool= new Pool(config);
+app.get('/test-db', function(req, res){
+    pool.query('select * from user',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(rsult));
+        }
+    });
 });
 
 var c=0;
