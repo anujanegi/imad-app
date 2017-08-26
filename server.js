a6ccc796163bdbb85cac19d31b1bbd827d527cf7
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-26 20:23:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt){
-    var hashed=crypto.pbkdf2Sync(input, salt,10000, 512, 'sha512' );
-    return hashed.toString('hex');
+    var hashed=crypto.pbkdf2Sync(input, salt,10000, 512, 'sha512' );//password based key derivation funcion
+    return ['pbkd',"10000",salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req,res){
