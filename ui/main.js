f0ed7b09f729474f9c21de59088421d821b8c768
ANUJA NEGI <anujanegi1@gmail.com>
2017-08-26 23:46:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,6 @@ var submit=document.getElementById('submit_btn');
 
 submit.onclick=function(){
     var request=new XMLHttpRequest();
-    
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200)
